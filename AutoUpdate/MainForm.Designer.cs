9679d8d77b968c570970018cb8b2786ebe198ad9
athit <athit305@gmail.com>
2017-12-15 11:31:58 +0700

    Initial commit

---
+++ AutoUpdate/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace AutoUpdate
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsFileName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsSpring = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsTotal = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsFileName,
+            this.tsSpring,
+            this.tsTotal});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 40);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(384, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsFileName
+            // 
+            this.tsFileName.Name = "tsFileName";
+            this.tsFileName.Size = new System.Drawing.Size(60, 17);
+            this.tsFileName.Text = " FileName";
+            // 
+            // tsSpring
+            // 
+            this.tsSpring.Name = "tsSpring";
+            this.tsSpring.Size = new System.Drawing.Size(285, 17);
+            this.tsSpring.Spring = true;
+            this.tsSpring.Text = " ";
+            // 
+            // tsTotal
+            // 
+            this.tsTotal.Name = "tsTotal";
+            this.tsTotal.Size = new System.Drawing.Size(24, 17);
+            this.tsTotal.Text = "0/0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(360, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 62);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Update";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsFileName;
+        private System.Windows.Forms.ToolStripStatusLabel tsSpring;
+        private System.Windows.Forms.ToolStripStatusLabel tsTotal;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ AutoUpdate/MainForm.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoUpdate
+{
+    public partial class MainForm : Form
+    {
+        private string ClientDir;
+        private string ServerDir;
+        private bool RunbyUser = true;
+        private List<string> FileUpdateList = new List<string>();
+        private string AppFileName = "OIS.exe";
+        private string ConfigFileName = "config.ini";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (ReadConfig())
+            {
+            }
+        }
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!backgroundWorker1.IsBusy)
+                {
+                    Thread.Sleep(2000);
+                    if (OpenConection())
+                    {
+                        GetUpdateFileList();
+                    }
+
+                    progressBar1.Maximum = FileUpdateList.Count;
+                    progressBar1.Step = 1;
+                    progressBar1.Value = 0;
+
+                    backgroundWorker1.RunWorkerAsync();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            UpdateFile();
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            //tsTotal.Text = string.Format("{0}/{1}", e.ProgressPercentage, FileUpdateList.Count);
+            //tsFileName.Text = FileUpdateList[e.ProgressPercentage - 1];
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            RunApp();
+        }
+
+        private bool ReadConfig()
+        {
+            try
+            {
+                ClientDir = Path.Combine("C:\\", ConfigurationManager.AppSettings["ClientFilePath"]);
+                ServerDir = ConfigurationManager.AppSettings["ServerFilePath"];
+                if(Environment.MachineName == "ATHIT0U0N15")
+                {
+                    RunbyUser = false;
+                    ServerDir = ConfigurationManager.AppSettings["ServerFilePath2"];
+                }
+
+                if(!Directory.Exists(ClientDir))
+                {
+                    Directory.CreateDirectory(ClientDir);
+                }
+                return true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+                return false;
+            }
+        }
+        private bool OpenConection()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+                return false;
+            }
+        }
+        private bool GetUpdateFileList()
+        {
+            try
+            {
+                if(ClientDir == null || ServerDir == null)
+                {
+                    return false;
+                }
+
+                NetworkCredential theNetworkCredential = new NetworkCredential(@"ERTC\Administrator", ConfigurationManager.AppSettings["CredentialPass"].ToString());
+                //CredentialCache theNetCache = new CredentialCache();
+                //theNetCache.Add(new Uri(@"\\SERVER\\OIS\\AutoUpdate"), "Basic", theNetworkCredential);
+                //theNetCache.Add(new Uri(@"\\SERVER\\OIS\\Deploy\OIS"), "Basic", theNetworkCredential);
+                //theNetCache.Add(new Uri(ServerDir), "Basic", theNetworkCredential);
+                string[] serverFileList;
+                if (!RunbyUser)
+                {
+                    using (new NetworkConnection(ServerDir, theNetworkCredential))
+                    {
+                        serverFileList = Directory.GetFiles(ServerDir, "*.*", SearchOption.AllDirectories);
+                    }
+                }
+                else
+                {
+                    serverFileList = Directory.GetFiles(ServerDir, "*.*", SearchOption.AllDirectories);
+                }
+                var clientFileList = Directory.GetFiles(ClientDir, "*.*", SearchOption.AllDirectories);
+
+                foreach (string serverFile in serverFileList)
+                {
+                    string sFile = serverFile.Substring(ServerDir.Length + 1);
+                    int index = sFile.LastIndexOf("\\");
+                    string sFolder = "";
+                    if(index >= 0)
+                    {
+                        sFolder = sFile.Substring(0, index);
+                    }
+
+                    string cFile = Path.Combine(ClientDir, sFile);
+                    string cFolder = Path.Combine(ClientDir, sFolder);
+
+                    if(sFolder != "" && !Directory.Exists(cFolder))
+                    {
+                        Directory.CreateDirectory(cFolder);
+                    }
+                    if (File.Exists(cFile))
+                    {
+                        FileInfo cFi = new FileInfo(cFile);
+                        FileInfo sFi = new FileInfo(serverFile);
+                        if(cFi.Length != sFi.Length || cFi.LastWriteTime != sFi.LastWriteTime)
+                        {
+                            if (sFi.Name != ConfigFileName)
+                            {
+                                FileUpdateList.Add(sFile);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        FileUpdateList.Add(sFile);
+                    }
+                }
+
+                foreach (string clientFile in clientFileList)
+                {
+                    string cFile = clientFile.Substring(ClientDir.Length + 1);
+                    bool isFoundOnServer = false;
+                    foreach (string serverFile in serverFileList)
+                    {
+                        string sFile = serverFile.Substring(ServerDir.Length + 1);
+                        if(cFile == sFile)
+                        {
+                            isFoundOnServer = true;
+                        }
+                    }
+                    if(!isFoundOnServer && cFile.Substring(0,10) != "AutoUpdate")
+                    {
+                        File.Delete(clientFile);
+                    }
+                }
+
+                tsFileName.Text = string.Empty;
+                tsTotal.Text = string.Format("0/{0}", FileUpdateList.Count);
+
+                return true;
+            }
+             catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void UpdateFile()
+        {
+            try
+            {
+                if(FileUpdateList.Count > 0)
+                {
+                    int i = 0;
+                    foreach(string FilePath in FileUpdateList)
+                    {
+                        tsFileName.Text = FileUpdateList[i];
+                        string ClientFilePath = Path.Combine(ClientDir, FilePath);
+                        string ServerFilePath = Path.Combine(ServerDir, FilePath);
+                        File.Copy(ServerFilePath, ClientFilePath, true);
+                        backgroundWorker1.ReportProgress(i + 1);
+                        tsTotal.Text = string.Format("{0}/{1}", i + 1, FileUpdateList.Count);
+                        i++;
+                    }
+                }
+
+                DeleteDirectory(ClientDir);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+        private void RunApp()
+        {
+            try
+            {
+                string FilePath = Path.Combine(ClientDir, AppFileName);
+                if(File.Exists(FilePath))
+                {
+                    Process.Start(FilePath);
+                    this.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+        private void DeleteDirectory(string startLocation)
+        {
+            foreach (var directory in Directory.GetDirectories(startLocation))
+            {
+                DeleteDirectory(directory);
+                if (Directory.GetFiles(directory).Length == 0 && Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+    }
+}
